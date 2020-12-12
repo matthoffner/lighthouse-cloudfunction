d0d9903f2fc088729826f0abecf2751dd42e28fb
Matt Hoffner <matth@porch.com>
2020-12-12 10:58:07 -0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# lighthouse-cloudfunction
+
+This should help anyone add lighthouse via a cloud function as well as cover some more advanced use cases.
+
+```sh
+gcloud functions deploy lighthouse --runtime nodejs12 --trigger-http --memory=2048MB --set-env-vars SECRET=mysecret
+```
+
+## Advanced usage:
+
+### Getting a Static IP
+
+Staging environments may require a static IP to be allowlisted. This is pretty straightforward with this guide:
+
+![AlvarDev: cloud functions static ip guide](https://github.com/AlvarDev/functions-static-ip)
---
+++ src/index.js
@@ -0,0 +1,28 @@
+const lighthouse = require("lighthouse");
+const puppeteer = require('puppeteer');
+
+const init = async () => await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+});
+
+const handle = async (url) => {
+    const browser = await init();
+    const res = await lighthouse(url, {
+        port: new URL(browser.wsEndpoint()).port,
+        output: 'html'
+    });
+    browser.close();
+    return res.report;
+}
+
+exports.lighthouse = async (request, response) => {
+    if (request.query.apiKey === process.env.SECRET) {
+        const data = await handle(request.query.url);
+        response
+          .status(200)
+          .send(data);
+      } else {
+        response.status(400).send("API key is not valid");
+    }
+}
